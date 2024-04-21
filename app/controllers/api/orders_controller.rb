d1b1cd4ee714e0e87eea4534c3cc0d43c9e1a2de
sanket1.tikhande <sanket1.tikhande@ril.com>
2024-04-21 11:20:59 +0530

    added new changes for order items

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,18 +1,82 @@
 class Api::OrdersController < ApplicationController
-
+  before_action :available_items, only: [:library_items, :order_item]
   def library_items
     @items = Item.all
     render json: @items.as_json(only: [:id,:title,:genre,:category])
   end
 
-  def order_items
-    Item.where(id: params["selected_items"]).update(user_id: params["user_id"])
-    render json: { message: 'Order has been placed  successfully' }
+  # def order_items
+  #   Item.where(id: params["@items_without_user"]).update(user_id: params["user_id"])
+  #   render json: { message: 'Order has been placed  successfully' }
+  # end
+  #
+  # def return_items
+  #   Item.where(id: params["returned_items"]).update(user_id:nil)
+  #   render json: { message: 'Items returned successfully' }
+  # end
+
+
+  def order_item
+    user = User.find(params[:user_id])
+    item = Item.find_by(title: params[:title], is_available: true)
+
+    available_items = @available_items & params[:order_items  ]
+    un_available_items = params[order_items] & @available_items
+
+
+    if user.transactions.where('date >= ?', 30.days.ago).count >= 10
+      render json: { error: 'Transaction limit exceeded' }, status: :bad_request
+      return
+    end
+
+    if !un_available_items.nil?
+      items = Item.where(id:un_available_items).pluck(:title)
+      render json: { message:  items.join(",")+'are not available' }, status: :ok
+      return
+    end
+
+    if item.genre == 'Crime' && user.age < 18
+      render json: { error: 'Age restriction' }, status: :bad_request
+      return
+    end
+
+    if user.transactions.where(item_id: item.id).count >= user.subscription.max_books
+      render json: { error: 'Max books reached for subscription' }, status: :bad_request
+      return
+    end
+
+    transaction = Transaction.new(user_id: user.id, item_id: item.id)
+    if transaction.save
+      items = Item.where(id:available_items).pluck(:title)
+      render json: { message: items.join(",")+'Ordered successfuly' }, status: :ok
+    else
+      render json: { error: 'Failed to create transaction' }, status: :unprocessable_entity
+    end
   end
 
-  def return_items
-    Item.where(id: params["returned_items"]).update(user_id:nil)
-    render json: { message: 'Order returned successfully' }
+  def return_item
+    user = User.find(params[:user_id])
+    items = Item.where(title: params[:titles])
+
+    items.each do |item|
+      transaction = user.transactions.find_by(item_id: item.id)
+
+      if transaction.nil?
+        render json: { error: "Item #{item.title} not borrowed by user" }, status: :bad_request
+        return
+      end
+
+      transaction.destroy
+      item.update(is_available: true)
+    end
+
+    render json: { message: 'Return successful' }, status: :ok
+  end
+
+
+private
+  def avaibile_items
+    @available_items = Item.where(user_id: nil).pluck(:id)
   end
 
 end
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @subscription_gold = subscriptions(:gold)
+    @user = users(:one)
+    @item1 = items(:book1)
+    @item2 = items(:magazine1)
+  end
+
+  test "should order item" do
+    post order_url, params: { user_id: @user.id, selected_items: [1,2,3] }
+    assert_response :success
+    assert_equal 'Order successful', JSON.parse(response.body)['message']
+  end
+
+  test "should return item" do
+    post order_url, params: { user_id: @user.id, selected_items: [1,2,3] }
+    post return_url, params: { user_id: @user.id,returned_items: [1,2,3] }
+    assert_response :success
+    assert_equal 'Return successful', JSON.parse(response.body)['message']
+  end
 end
