c4acf98d5f51d4ad2875ea8ade2aa7137ccf3500
Sanket Tikhande <tikhandesanket@users.noreply.github.com>
2024-04-22 11:47:19 +0530

    Added new changes for borrow and update request

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,37 +1,15 @@
-class Api::OrdersController < ApplicationController
-  before_action :available_items, only: [:library_items, :order_item]
-  def library_items
-    @items = Item.all
-    render json: @items.as_json(only: [:id,:title,:genre,:category])
-  end
-
-  # def order_items
-  #   Item.where(id: params["@items_without_user"]).update(user_id: params["user_id"])
-  #   render json: { message: 'Order has been placed  successfully' }
-  # end
-  #
-  # def return_items
-  #   Item.where(id: params["returned_items"]).update(user_id:nil)
-  #   render json: { message: 'Items returned successfully' }
-  # end
-
-
+class OrdersController < ApplicationController
   def order_item
     user = User.find(params[:user_id])
     item = Item.find_by(title: params[:title], is_available: true)
-
-    available_items = @available_items & params[:order_items  ]
-    un_available_items = params[order_items] & @available_items
-
 
     if user.transactions.where('date >= ?', 30.days.ago).count >= 10
       render json: { error: 'Transaction limit exceeded' }, status: :bad_request
       return
     end
 
-    if !un_available_items.nil?
-      items = Item.where(id:un_available_items).pluck(:title)
-      render json: { message:  items.join(",")+'are not available' }, status: :ok
+    if item.nil?
+      render json: { error: 'Item not available' }, status: :bad_request
       return
     end
 
@@ -47,8 +25,7 @@ class Api::OrdersController < ApplicationController
 
     transaction = Transaction.new(user_id: user.id, item_id: item.id)
     if transaction.save
-      items = Item.where(id:available_items).pluck(:title)
-      render json: { message: items.join(",")+'Ordered successfuly' }, status: :ok
+      render json: { message: 'Order successful' }, status: :ok
     else
       render json: { error: 'Failed to create transaction' }, status: :unprocessable_entity
     end
@@ -72,11 +49,4 @@ class Api::OrdersController < ApplicationController
 
     render json: { message: 'Return successful' }, status: :ok
   end
-
-
-private
-  def avaibile_items
-    @available_items = Item.where(user_id: nil).pluck(:id)
-  end
-
 end
